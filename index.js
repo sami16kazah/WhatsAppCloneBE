8131d779c9df7d93a8be598cc697d1150e47c53b
sam16kazah <samkazah444@gmail.com>
2023-12-06 13:11:10 +0300

    adding UnCautghtex+ Unhandedrej errors + Sigterm

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+import { log } from 'winston';
 import app from './app.js';
 import logger from './configs/logger.js';
 const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
+let server;
+server = app.listen(PORT, () => {
   logger.info(`server is running at port ${PORT}`);
+});
+
+//handle server error
+const exitHandler = () => {
+  if (server) {
+    logger.info('serverClosed');
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexprectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexprectedErrorHandler);
+process.on('unhandledRejection', unexprectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  if (server) {
+    logger.info('serverClosed');
+    process.exit(1);
+  }
 });
