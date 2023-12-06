6b272093f5fd6a0c98f3c75d1b2dbe416dcf24c3
sam16kazah <samkazah444@gmail.com>
2023-12-06 16:30:18 +0300

    adding db

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import app from './app.js';
 import logger from './configs/logger.js';
+import mongoose from 'mongoose';
+
+const { MONGODB_URI } = process.env;
 const PORT = process.env.PORT;
+// exit on mongodb error
+mongoose.connection.on('error', (err) => {
+  logger.error(`MongoDb Connection Error : ${err}`);
+  process.exit(1);
+});
+//mongo db debug mode
+if (process.env.NODE_ENV !== 'production') {
+  mongoose.set('debug', true);
+}
+mongoose.connect(MONGODB_URI).then(logger.info('connected to mongo'));
 let server;
 server = app.listen(PORT, () => {
   logger.info(`server is running at port ${PORT}`);
