ff1b970fcc3e53885af41ef6f4734ca595edeafd
sam16kazah <samkazah444@gmail.com>
2023-12-18 10:00:15 +0300

    fix empty messages

--- SocketServer.js
+++ SocketServer.js
@@ -5,7 +5,6 @@ export default function (socket, io) {
     socket.join(user);
     if (!onlineUsers.some((u) => u.userId === user)) {
       onlineUsers.push({ userId: user, socketId: socket.id });
-      console.log('online users are', onlineUsers);
     }
     io.emit('get-online-users', onlineUsers);
   });
@@ -17,12 +16,13 @@ export default function (socket, io) {
   //join a converstion room
   socket.on('join conversation', (conversation) => {
     socket.join(conversation);
-    console.log('user has joined conv :', conversation);
   });
 
   socket.on('send message', (message) => {
     let converstion = message.conversation;
-    if (!converstion.users) return;
+    if (!converstion || !converstion.users) {
+      return;
+    }
     converstion.users.forEach((user) => {
       if (user._id === message.sender._id) return;
       socket.in(user._id).emit('receive message', message);
